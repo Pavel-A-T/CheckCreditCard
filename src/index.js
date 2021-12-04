cd3c1d40ffcf2fb0625648b937f0b3de45199e87
Pavel Titov <otrepiev.grishka@yandex.ru>
2021-12-04 17:15:39 +0300

    сделал lint-fix

--- src/index.js
+++ src/index.js
@@ -10,17 +10,17 @@ btn.addEventListener('click', (event) => {
   event.preventDefault();
   if (input.value) {
     Array.from(document.getElementsByClassName('card'))
-      .forEach(item => item.classList.remove('card-disabled'));
-    notValid.classList.add("show");
-    valid.classList.add("show");
-    valid.innerText = "Valid";
+      .forEach((item) => item.classList.remove('card-disabled'));
+    notValid.classList.add('show');
+    valid.classList.add('show');
+    valid.innerText = 'Valid';
     if (checkPaymentSystem(input.value) === 'nothing') {
       notValid.classList.remove('show');
     } else {
       const card = checkPaymentSystem(input.value);
       valid.classList.remove('show');
       valid.innerText += ` ${input.value} - This is ${card}!`;
-      input.value = "";
+      input.value = '';
       Array.from(document.getElementsByClassName('card')).filter((item) => !item.classList.contains(card))
         .forEach((item) => item.classList.add('card-disabled'));
     }
